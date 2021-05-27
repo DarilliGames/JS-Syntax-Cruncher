647d3b892938aafabb51d15e50214c5d93738e8a
Stephen Moody <darilligames@gmail.com>
2021-05-27 12:00:04 +0000

    Created functionality for basic defining functions

--- scripts/jsDefFunc1.js
+++ scripts/jsDefFunc1.js
@@ -3,26 +3,33 @@ let challenge = "jsDefFunc1";
 let operation;
 
 let listOfOperators = [
-    ["turnOn", ["lightOn", "true"],  ["let lightOn = false;"], "lightOn=true"],
+    ["turnOn", ["lightOn", "true"], ["let lightOn = false;"], "lightOn=true"],
     ["turnOff", ["lightOn", "false"], ["let lightOn = true;"], "lightOn=false"],
     ["setZero", ["x", "0"], ["let x = 999;"], "x=0"],
     ["makeRed", ["color", "red"], ["let red = #ff0000;", "let blue = #0000ff;", "let color;"], "color=red"],
     ["makeBlue", ["color", "blue"], ["let red = #ff0000;", "let blue = #0000ff;", "let color;"], "color=blue"]
-    
+
 ];
 
 
 function newChallenge() {
     //  Generate conditions which the student must meet
     let logicGen = Math.floor(Math.random() * listOfOperators.length);
-    operation = listOfOperators[logicGen];    
+    operation = listOfOperators[logicGen];
     let infoText = "";
     operation[2].forEach(item => infoText += item + "\n");
     // Clear the feedback given to the student
     $("#textBack").text("");
-    $("#codeArea").val(infoText+"\n");
+    $("#codeArea").val(infoText + "\n");
     $("#instructions").text(`Define a function called ${operation[0]}.  Within the function it should
     set the variable ${operation[1][0]} equal to ${operation[1][1]}`);
+
+}
+
+function checkString(should, current) {
+    current = current.substring(0, should.length);
+
+    return (current == should);
 
 }
 
@@ -32,35 +39,53 @@ function getTextArea() {
     console.log(operation[2].length);
     codeIn = codeIn.filter(item => item !== "");
     console.log(codeIn);
-    codeIn = codeIn.splice(0, 1);
+    codeIn = codeIn.splice(operation[2].length);
     console.log(codeIn);
 
-    let stringCheck = "";
-    let conditionChecks = [
-        [`function`, "Did you define the function with the keyword 'function'"],
-        [``, "The functions name must be then defined"],
-        [`=`, "The Variable needs to be defined as 'x'"],
-        
-        
 
+    let conditionChecks = [
+        [
+            [`function`, "Did you define the function with the keyword 'function'"],
+            [`${operation[0]}`, "The functions name must be then defined"],
+            [`()`, "Did you remember the empty brackets for no parameters?"],
+            [`{`, "Did you open the curly bracket"]
+
+        ],
+        [
+            [`${operation[1][0]}`, `We want to set the variable ${operation[1][0]} to a value.`],
+            [`=`, `You need to use = to set a value`],
+            [`${operation[1][1]}`, `You are required to set the value of ${operation[1][0]} to ${operation[1][1]}`],
+            [`;`, `Remember to close the line of code with a ;`],
+
+        ],
+        [
+            [
+                "}", "Did you remember to add a closing curly bracket? }"
+            ]
+        ]
     ];
     if (typeof (codeIn[0]) == "undefined") {
         $("#textBack").text("Start typing your variable!");
         return;
     } else {
-
         for (let i = 0; i < conditionChecks.length; i++) {
-            stringCheck = stringCheck + conditionChecks[i][0];
-            if (!checkString(stringCheck, codeIn[0])) {
-
-                $("#textBack").text(conditionChecks[i][1]);
-                return
+            let stringCheck = "";
+            for (let j = 0; j < conditionChecks[i].length; j++) {
+                stringCheck = stringCheck + conditionChecks[i][j][0];
+                if (codeIn[i]) {
+                    if (!checkString(stringCheck, codeIn[i])) {
+                        console.log(stringCheck, codeIn[i])
+                        $("#textBack").text(conditionChecks[i][j][1]);
+                        return
+                    }
+                }else{
+                    $("#textBack").text("You need to continue the code onto the next line");
+                    return
+                }
             }
 
         }
-
     }
-    
     if (speedMode && timeLeft > 0) {
         score++;
         $("#score").text(score);
